67eefbf19e6cd7d3412dd504a0a225a29db53b82
Christiaan Bloemendaal <chrisbloemendaal@gmail.com>
2019-12-01 16:37:39 +0100

    ADD Day One - Puzzle Two project and implementation

---
+++ .NET Core/Day One/Puzzle Two/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TNRD.AdventOfCode.DayOne.PuzzleTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Foundation.PuzzleSolver.Solve(1);
+        }
+    }
+}
