3bbe51784359e958f9a6cbf4bf5aaab4d2a4cb90
Christiaan Bloemendaal <chrisbloemendaal@gmail.com>
2019-12-04 19:36:05 +0100

    ADD Day Four projects

---
+++ .NET Core/Day Four/Puzzle Two/Program.cs
@@ -0,0 +1,12 @@
+﻿using TNRD.AdventOfCode.Foundation;
+
+namespace TNRD.AdventOfCode.DayFour.PuzzleTwo
+{
+    class Program : BaseProgram
+    {
+        static void Main(string[] args)
+        {
+            CreateAndSolve();
+        }
+    }
+}
---
+++ .NET Core/Day Four/Puzzle Two/PuzzleSolver.cs
@@ -0,0 +1,12 @@
+namespace TNRD.AdventOfCode.DayFour.PuzzleTwo
+{
+    public class PuzzleSolver : Foundation.PuzzleSolver
+    {
+        public override int Day => 4;
+
+        public override object Solve(string input)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
