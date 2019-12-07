1f803afff8cc6eb732d876849650c79650af5cd8
Christiaan Bloemendaal <chrisbloemendaal@gmail.com>
2019-12-07 14:05:46 +0100

    ADD Day Seven projects

---
+++ .NET Core/Puzzle One/Program.cs
@@ -0,0 +1,12 @@
+﻿using TNRD.AdventOfCode.Foundation;
+
+namespace TNRD.AdventOfCode.DaySeven.PuzzleOne
+{
+    class Program : BaseProgram
+    {
+        static void Main(string[] args)
+        {
+            CreateAndSolve();
+        }
+    }
+}
---
+++ .NET Core/Puzzle One/PuzzleSolver.cs
@@ -0,0 +1,12 @@
+namespace TNRD.AdventOfCode.DaySeven.PuzzleOne
+{
+    public class PuzzleSolver : Foundation.PuzzleSolver
+    {
+        public override int Day => 7;
+
+        public override object Solve(string input)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
