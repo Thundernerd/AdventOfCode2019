7e32cb9a10930083c6935f9e8b6c3ca7873918ac
Christiaan Bloemendaal <chrisbloemendaal@gmail.com>
2019-12-07 12:47:09 +0100

    ADD Day Six projects

---
+++ .NET Core/Day Six/Puzzle Two/PuzzleSolver.cs
@@ -0,0 +1,12 @@
+namespace TNRD.AdventOfCode.DaySix.PuzzleTwo
+{
+    public class PuzzleSolver : Foundation.PuzzleSolver
+    {
+        public override int Day => 6;
+
+        public override object Solve(string input)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
